2e3642b17fb6aa46935bdb37bf70563482ebd139
outblue <1418849293@qq.com>
2016-02-01 20:52:26 +0800

    提交。

---
+++ A+B.Format.1001/1001.A+B Format (20).c
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+#include<math.h>
+int main()
+{
+	int a,b,c,i,j,r;
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	i=0;
+	if(abs(c)<1000) printf("%d",c);
+	else if(abs(c)<1000000)
+	{
+		i=c%1000;
+		j=c/1000;
+		printf("%d,%03d",j,abs(i));
+	}
+	else
+	{
+		i=c%1000;
+		c=c/1000;
+		j=c%1000;
+		r=c/1000;
+		printf("%d,%03d,%03d",r,abs(j),abs(i));
+	}
+	return 0;
+}
+
+
+
